8752f01ac181a0b6ea8d0273630910c237f6c818
Mücahit Cihat Ç <119215395+Mucahitcc@users.noreply.github.com>
2025-03-07 02:17:34 +0300

    Proje 6: Metotlar (Methods)
    Kodlarımı daha düzenli ve tekrar kullanılabilir hale getirmek için metotlar oluşturmayı ve parametre alarak işlem yapan ve sonuç döndüren metotlar yazmayı denedim.

---
+++ CSharpEgitimKampi-master/06_Arrays/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Dizilerde Örnekler VE metotlar
+            //int[] myArray = { 47, 85, 95, 41, 25, 65, 95, 278, 365 };
+            //int maxNumber = myArray[0];
+
+            //for (int i=0; i<myArray.Length; i++)
+            //{
+            //    if (myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //    Console.WriteLine(maxNumber);
+            //}
+
+            //Küçükten büyüğe sıralama
+            //int[] numbers = { 41, 74, 15, 55, 28, 37,  63, 88, };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+
+            //// diziye tersten sıralıyor
+            //int[] numbers = { 41, 74, 15, 55, 28, 37, 63, 88, };
+            //Array.Reverse(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //İndex sırasını bulma
+            //string[] customer = { "ali", "mehmet", "hasan", "sait", "akif" };
+            //int index = Array.IndexOf(customer, "hasan");
+            //Console.WriteLine(index);
+
+            //int[] numbers ={ 45,566,656,66,78,98,12};
+            //Console.WriteLine("dizinin en büüyk elemanı:"+" "+numbers.Max()+" "+"dizinin en küçük elemenı:"+ " "+numbers.Min());
+
+
+
+
+            #endregion
+
+            #region kullanıcıdan değer alma
+            //string[] cities = new string[5];
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.WriteLine($"Lütfen {i+1}.Şehri Giriniz");
+            //    cities[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("----------------------");
+
+            //for (int i = 0;i < cities.Length; i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+
+            //int[] numbers = { 10, 20, 35, 63, 32, 56, 65, 95 };
+            //int sum = 0;    
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //int[] numbers = { 10, 20, 35, 63, 32, 56, 65, 95 };
+            //Console.WriteLine("Çift Sayılar");
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+
+            //}
+            //Console.WriteLine("-----------");
+            //Console.WriteLine("Tek sayılar");
+            //Console.WriteLine();
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 1)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+
+            //}
+            #endregion
+            Console.ReadLine();
+        }
+
+    }
+}
+
